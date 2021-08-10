282a8db28ec43ad15f75fc32402406817eb7f6a1
binhpham <binh.pham@limeade.com>
2021-08-09 22:22:00 -0700

    more leetcode

--- leetcode/database/1083.sql
+++ leetcode/database/1083.sql
@@ -62,7 +62,7 @@ from Sales s
 join Product p on p.product_id = s.product_id
 where p.product_name = 'S8'
 and buyer_id not in (
-    select buyer_id 
+    select distinct buyer_id 
     from Sales s
     join Product p on p.product_id = s.product_id
     where p.product_name = 'iPhone'
